3e6c1b2fb92fc9ba1b72a5603cb7dfd74b6064d5
eunsang.kim <gomuzom@naver.com>
2022-11-19 15:45:37 +0900

    feat: picture 모델 생성

---
+++ lib/domain/model/picture.g.dart
@@ -0,0 +1,18 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'picture.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+_$_Picture _$$_PictureFromJson(Map<String, dynamic> json) => _$_Picture(
+      url: json['url'] as String,
+      tag: json['tag'] as String,
+    );
+
+Map<String, dynamic> _$$_PictureToJson(_$_Picture instance) =>
+    <String, dynamic>{
+      'url': instance.url,
+      'tag': instance.tag,
+    };
